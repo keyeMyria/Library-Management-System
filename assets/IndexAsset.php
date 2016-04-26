16bdc5495db1031aa39977bb6ba49f92a5a6db96
smartMao <664643875@qq.com>
2016-04-26 11:20:44 +0800

    增加前端资源文件 IndexAsset.php ，服务于路由 index/index (主页)

---
+++ assets/IndexAsset.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Zhiqiao Xu  <xuzhiqiao97@gmail.com> 
+ */
+
+class IndexAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+
+    public $css = [
+
+    ];
+
+    public $js = [
+		'js/jquery/jquery-1.12.3.min.js',
+		'js/layer/layer.js',
+    ];
+
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+
+}
