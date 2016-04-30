c444dad807534ce35adb7a0d344f4db21ed217ba
smartMao <664643875@qq.com>
2016-04-30 09:54:43 +0800

    书架管理模块 的 主页 增加 tip　

--- views/bookshelf/index.php
+++ views/bookshelf/index.php
@@ -1,13 +1,31 @@
 <?php
 
 use app\assets\BookshelfAsset;
+use app\assets\LayerGlobalAsset;
+
 use yii\widgets\ActiveForm;
-use yii\Helpers\Html;
 use yii\widgets\LinkPager;
 
+use yii\Helpers\Url;
+use yii\Helpers\Html;
+
 BookshelfAsset::register( $this );
+LayerGlobalAsset::register( $this );
+
+
+/** -------------------------------------------------------
+ * 判断是否出现 " 操作成功 " 的 tip 层
+ * @var $session['isShowTip'] boolean  为 true 则出现 tip 层，false反之
+ */
+if ( $session['isShowTip'] ){
+	echo " <script> window.onload = function(){ layer.msg('操作成功', { icon: 1, offset:'100px'}) } </script>";
+	$session['isShowTip'] = false;
+}
 
 ?>
+
+
+
 
 
 <div class="all">
