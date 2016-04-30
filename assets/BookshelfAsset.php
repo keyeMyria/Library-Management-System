7833c9c3c349a57d350f6794f69f26de7451d2ae
smartMao <664643875@qq.com>
2016-04-30 09:44:25 +0800

    完善了部分 asset 包的所属说明

--- assets/BookshelfAsset.php
+++ assets/BookshelfAsset.php
@@ -5,6 +5,7 @@ namespace app\assets;
 use yii\web\AssetBundle;
 
 /**
+ * 此 asset 只服务于书架管理模块
  * @author Zhiqiao Xu  <xuzhiqiao97@gmail.com> 
  */
 
--- assets/LoginAsset.php
+++ assets/LoginAsset.php
@@ -5,6 +5,7 @@ namespace app\assets;
 use yii\web\AssetBundle;
 
 /**
+ * 此 asset 只服务于用户登陆模块
  * @author Zhiqiao Xu  <xuzhiqiao97@gmail.com> 
  */
 
