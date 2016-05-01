d38c3a96f29c3cfd11d320e4f04bdf0f0749c7db
smartMao <664643875@qq.com>
2016-05-01 22:50:21 +0800

    添加 读者类型管理模型

---
+++ models/ReaderType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+
+
+
+class ReaderType extends ActiveRecord
+{
+
+	public static function tableName()
+	{
+		return 'lib_readerType';	
+	}
+
+}
