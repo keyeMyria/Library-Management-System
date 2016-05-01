6db94899687d2b0c3ecf5e0937986d5ef3cabc35
smartMao <664643875@qq.com>
2016-05-01 22:48:41 +0800

    读者类型管理模块 新增 Index 首页action

--- controllers/ReaderTypeController.php
+++ controllers/ReaderTypeController.php
@@ -4,11 +4,21 @@ namespace app\controllers;
 
 use yii\web\Controller;
 
+use app\models\ReaderType;
+
+
 
 class ReaderTypeController extends Controller
 {
 
 	
+	public function actionIndex()
+	{
+		$readerTypeModel = new ReaderType;		
+		return $this->render('index', [
+			'model' => $readerTypeModel,	
+		]);	
+	}
 
 }
 
