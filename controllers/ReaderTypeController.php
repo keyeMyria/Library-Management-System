bcb63d4f604bae0a16205a92971f95cfde0e5d16
smartMao <664643875@qq.com>
2016-05-02 22:20:06 +0800

    增加读者类型管理 首页、 添加功能

--- controllers/ReaderTypeController.php
+++ controllers/ReaderTypeController.php
@@ -3,6 +3,10 @@
 namespace app\controllers;
 
 use yii\web\Controller;
+use yii\web\Session;
+use yii\db\Query;
+use yii\data\Pagination;
+use Yii;
 
 use app\models\ReaderType;
 
@@ -11,14 +15,184 @@ use app\models\ReaderType;
 class ReaderTypeController extends Controller
 {
 
+	public $defaultPageSize = 10;
 	
 	public function actionIndex()
 	{
-		$readerTypeModel = new ReaderType;		
-		return $this->render('index', [
-			'model' => $readerTypeModel,	
-		]);	
+		$request = Yii::$app->request;
+		if ( $post = $request -> post() ){
+
+
+			$this -> actionAddReaderType( $post );
+
+		} else {
+
+			$session = new Session;
+			$model   = new ReaderType;
+			$query  = (new Query) 
+				-> select(['PK_readerTypeID', 'readerTypeName', 'readerTypeBorrowNumber']) 
+				-> orderBy('PK_readerTypeID DESC') 
+				-> from( $model -> tableName() );
+			$cloneQuery = clone $query;
+		
+			$pages = new Pagination(['totalCount' => $cloneQuery->count() ]);
+			$pages -> defaultPageSize =  $this -> defaultPageSize;
+			$data = $query -> offset( $pages->offset ) 
+						   -> limit( $pages->limit ) 
+					       -> all();
+
+			return $this->render('index', [
+				'model'		=> $model,
+				'data'		=> $data,
+				'pages'		=> $pages,
+				'session'   => $session,
+
+			]);		
+		}
 	}
+
+
+
+	public function actionAddReaderType( $post )
+	{
+		$ReaderTypeModel = new ReaderType;
+		#$ReaderTypeModel -> bookshelfName = $post['Bookshelf']['bookshelfName'];
+		$ReaderTypeModel -> readerTypeName = $post['ReaderType']['readerTypeName'];		
+		$ReaderTypeModel -> readerTypeBorrowNumber = $post['ReaderType']['readerTypeBorrowNumber'];		
+	
+		$res = ReaderType::find()->where( ['readerTypeName' => $post['ReaderType']['readerTypeName'] ] ) -> one();
+		if( !empty($res) ){
+			// 输入的 读者类型名称 已存在
+			// 那就给个 tip 说明，并返回 index
+			dump( $res );exit;
+		}
+
+	
+		xit;
+		if( $ReaderTypeModel -> save() ){
+			$session = new Session;
+			$session['isShowTip'] = true;
+
+			return $this->redirect(['index']);
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
