22818a1701a4f14bad05ec70d5bce5e9a11459ca
smartMao <664643875@qq.com>
2016-05-04 21:08:42 +0800

    添加 全局下拉框 的使用说明 在 DropDownGlobalAsset 包内

--- assets/DropDownGlobalAsset.php
+++ assets/DropDownGlobalAsset.php
@@ -7,7 +7,22 @@ use yii\web\AssetBundle;
 /**
  * 此 asset 用于全局的下拉框功能， 提供 CSS JS  
  * @author Zhiqiao Xu  <xuzhiqiao97@gmail.com> 
- */
+ *
+ * 使用方法(以下说的代码，全都是写在 view 层)：
+ *	1. 首先注册资源包
+ *	2. 创建出 <select>  <option>...... 这些标签块
+ *	3. 在 <select> 给上属性 class='basic-usage-demo'
+ *	4. 随便找个地方，写上
+ *					<script>
+					     window.onload = function(){
+							$(document).ready(function(){
+								$('.basic-usage-demo').fancySelect();
+							});
+						 }
+					</script>
+
+*/
+
 
 class dropDownGlobalAsset extends AssetBundle
 {
