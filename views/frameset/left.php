4c986ecd3c221257607e51af5b3e82039bb959e8
smartMao <664643875@qq.com>
2016-05-05 21:32:26 +0800

    添加指向 图书搜索模块的 url

--- views/frameset/left.php
+++ views/frameset/left.php
@@ -31,7 +31,7 @@ FramesetLeftAsset::register( $this );
                 <li><a href="#"><img src="../images/frameset/left/books.png" alt="" /> 图书档案</a>
 					<ul>
                         <li><a href="<?=  Url::to(['book-add/book-add'])?>" target='right'>图书添加</a></li>
-                        <li><a href="#">图书搜索</a></li>
+                        <li><a href="<?=  Url::to(['book-sreach/sreach'])?>" target='right'>图书搜索</a></li>
                         <li><a href="#">图书信息统计</a></li>
                     </ul>
                 </li>
