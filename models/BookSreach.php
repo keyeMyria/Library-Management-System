f7342e592b28e723fa39c5466a102024eb1d0b97
smartMao <664643875@qq.com>
2016-05-08 22:54:09 +0800

    完成 图书搜索 中的 出版社搜索

--- models/BookSreach.php
+++ models/BookSreach.php
@@ -36,7 +36,7 @@ class BookSreach extends Model
 				break;
 
 			case 'publisher':
-
+				return $this -> sreachPublisher( $sreachText );	
 				break;
 
 			case 'author':
@@ -64,14 +64,49 @@ class BookSreach extends Model
 	public function sreachBookInfoPublic( $sreachType, $sreachText )
 	{
 		$db = Yii::$app->db;
-		$bookInfoTableName = BookInfo::tableName();
+		$bookInfoTableName = BookInfo::tableName(); 
 
-		$sql = "SELECT * FROM $bookInfoTableName  WHERE $sreachType LIKE  '%$sreachText%'";
+
+		$sql = "SELECT `PK_bookInfoID`, `bookInfoBookISBN`, `bookInfoBookName`, `bookInfoBookAuthor`  FROM $bookInfoTableName  WHERE $sreachType LIKE  '%$sreachText%'";
 		$result = $db -> createCommand( $sql ) -> queryAll();
 		
 		return $result;
 		
 	}
 
+
+	public function sreachPublisher( $sreachText )
+	{
+		$db = Yii::$app->db;
+		$publisherTableName        = Publisher::tableName();
+		$bookInfoTableName         = BookInfo::tableName(); 
+		$bookRelationshipTableName = BookInfo::bookRelationshipTableName();
+		
+		// 下面sql语句是 利用 publisher 表查询出的 id , 再去 join  bookRelationship表。
+		$sql = "SELECT `FK_bookInfoID` FROM $publisherTableName AS p JOIN $bookRelationshipTableName  AS r   ON p.PK_publisherID = r.FK_publisherID WHERE p.publisherName = '$sreachText'";
+
+		// 执行 sql 语句后, 查询到是该 $sreachText 出版社的图书的 ID, 
+		$bookInfoID  = $db -> createCommand( $sql ) -> queryAll();
+
+
+		// 有了图书ID 后就好办了, 直接查询后得到结果放入 $bookInfoResult 数组中。
+		foreach( $bookInfoID as $key => $value)
+		{
+			$id = $bookInfoID[$key]['FK_bookInfoID'];
+			$bookInfoSql = "SELECT * FROM $bookInfoTableName WHERE PK_bookInfoID = $id";	
+			$bookInfoResult[] = $db -> createCommand( $bookInfoSql ) -> queryAll();
+		}
+
+		// 由于上面 $bookInfoResult[] 数组插入时多了一层(没办法), 这个数组结构变成了 $arr[0][0][val];
+		// 但是，在 view 的输出的格式是要通用的 $arr[0][val];  所以就有了下面的代码, 把数组的结构去掉一层.
+
+		foreach( $bookInfoResult as $key => $value )
+		{
+			$result[] = $bookInfoResult[$key][0];	
+		}
+
+		return $result;
+	}
+	
 
 }
