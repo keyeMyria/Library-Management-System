d8326957885ea83fd67981d8c27fb92e1c2dd676
smartMao <664643875@qq.com>
2016-05-08 22:55:51 +0800

    在 BookInfo 模块中，增加一个为了获取 lib_bookRelationship 数据表名字的方法

--- models/BookInfo.php
+++ models/BookInfo.php
@@ -7,6 +7,12 @@ use yii\db\ActiveRecord;
 
 class BookInfo extends ActiveRecord
 {
+
+	public static function bookRelationshipTableName()
+	{
+		return 'lib_bookRelationship';
+	}
+
 	public static function tableName()
 	{
 		return 'lib_bookInfo';	
