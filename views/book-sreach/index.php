d30f48e4ae7e1022972b71360e61f81605ce5a51
smartMao <664643875@qq.com>
2016-05-09 13:30:32 +0800

    在 bookSreach 的 view 中完成了 图书搜索页面右上角 文字层的完美显示

--- views/book-sreach/index.php
+++ views/book-sreach/index.php
@@ -14,6 +14,17 @@ use yii\helpers\Html;
 DropDownGlobalAsset::register( $this );
 BookSreachAsset::register( $this );
 
+/** -------------------------------------------------------
+ * 判断是否出现 " 操作成功 " 的 tip 层
+ * @var $session['isShowTip'] boolean  为 true 则出现 tip 层，false反之
+ */
+if ( $session['isShowTip'] ){
+	echo " <script> function tip(){ layer.msg('{$session['tipContent']}', { icon: 1, offset:'100px'}) } </script>";
+	$session['isShowTip'] = false;
+}
+
+
+
 ?>
 
 <div class='all'>
@@ -30,7 +41,7 @@ BookSreachAsset::register( $this );
 	<div class="input-box">
 		<?= Html::beginForm() ?> 
 			<?= Html::dropDownList('sreachType', null, $sreachType ,['class' => 'basic-usage-demo']) ?>
-			<?= Html::input('text', 'sreach', null,  ['class' => 'form-control form-sreach'] ) ?>
+			<?= Html::input('text', 'sreachText', null,  ['class' => 'form-control form-sreach'] ) ?>
 			<?= Html::SubmitButton('搜索', ['class' => 'btn btn-primary sreach-btn']) ?>
 		<?= Html::endForm() ?>
 	</div>
@@ -39,10 +50,16 @@ BookSreachAsset::register( $this );
 	<!-- 页面右上角文字层  -->
 	<?php if( isset( $sreachResult )){ ?>
 	<div class="sreach-info">
-		<span class="sreach-tip">按书名 <span class="sreach-type">linux</span> 进行搜索</span>		
+		<span class="sreach-tip"><?php echo $sreachResultInfo['sreachType'];  ?>
+			<span class="sreach-type"> <?php echo $sreachResultInfo['sreachResultText']; ?> </span> 
+			进行搜索
+		</span>		
 		<br/>
 		<span class="sreach-result">
-		搜索结果<span class="sreach-result-number"> 5 </span>条数据, 在 <span class="sreach-spend-time">1.3</span> 秒内完成查询。
+			搜索结果
+					<span class="sreach-result-number"> <?php echo $sreachResultInfo['sreachResultCount'];  ?> </span>
+			条数据, 在 
+					<span class="sreach-spend-time"> <?php echo $sreachResultInfo['sreachResultTime'];  ?></span> 秒内完成查询。
 		</span>
 	</div>
 	<?php } ?>
@@ -65,6 +82,7 @@ BookSreachAsset::register( $this );
 			<?php
 		if( isset( $sreachResult) ){
 			foreach( $sreachResult as $key => $value ){ ?>
+
 				<tr>
 					<td><?php echo $sreachResult[$key]['bookInfoBookISBN']; ?></td>
 					<td><?php echo $sreachResult[$key]['bookInfoBookName']; ?></td>
@@ -94,6 +112,7 @@ BookSreachAsset::register( $this );
 	window.onload = function()
 	{
 		dropDown();
+		tip();
 	}
 
 	function dropDown()
