6e8de156eb3375022745d3138dea61422f5a5365
smartMao <664643875@qq.com>
2016-05-10 15:03:20 +0800

    图书搜索 模块 增加分页功能 在控制器部分

--- controllers/BookSreachController.php
+++ controllers/BookSreachController.php
@@ -4,6 +4,8 @@ namespace app\controllers;
 
 use yii\web\Controller;
 use yii\web\Session;
+use yii\data\ActiveDataProvider;
+use yii\data\Pagination;
 use Yii;
 
 
@@ -11,7 +13,8 @@ use app\models\BookSreach;
 
 class BookSreachController extends Controller
 {
-	
+	public $defaultPageSize = 5;
+
 	
 	public $defaultAction = 'sreach';
 	/**
@@ -46,21 +49,33 @@ class BookSreachController extends Controller
 
 				$session['isShowTip'] = false;
 
+				$sreachResult       = $bookSreachModel -> bookSreach( $post );
+				#$sreachResultInfo  = $bookSreachModel -> getSreachResultInfo();
+				
+					
+				$cloneQuery = clone $sreachResult;
 
-
-				$sreachResult      = $bookSreachModel -> bookSreach( $post );
-				$sreachResultInfo  = $bookSreachModel -> getSreachResultInfo();
+				$pages = new Pagination(['totalCount' => $cloneQuery->count() ] );
+				$pages -> defaultPageSize = $this -> defaultPageSize;
 
 				
+				$models = $sreachResult -> offset( $pages->offset ) -> limit( $pages->limit ) -> all();
+
+
+				
+
+
 
 			}
 
 			return $this -> render('index', [
+				'pages' => $pages,
+				'models' => $models,
 				'session'          => $session,
 				'sreachType'       => $sreachTypeArr,	
 				'sreachText'       => $sreachText,
 				'sreachResult'     => $sreachResult,	
-				'sreachResultInfo' => $sreachResultInfo,
+				#'sreachResultInfo' => $sreachResultInfo,
 			]);
 
 		} else {
