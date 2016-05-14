4472fdfd1e8aa894472e5239ef29ea17b683027f
smartMao <664643875@qq.com>
2016-05-14 21:26:53 +0800

    图书搜索结果 的 “编辑” 按钮 页面的 view 层

---
+++ views/book-sreach/edit.php
@@ -0,0 +1,108 @@
+<?php
+
+use app\assets\LayerGlobalAsset;
+use app\assets\BookAddAsset;
+use app\assets\DropDownGlobalAsset;
+use app\assets\BookSreachEditAsset;
+
+use yii\widgets\ActiveForm;
+use yii\widgets\LinkPager;
+
+use yii\helpers\Url;
+use yii\helpers\Html;
+
+LayerGlobalAsset::register( $this );
+BookAddAsset::register( $this );
+DropDownGlobalAsset::register( $this );
+BookSreachEditAsset::register( $this );
+
+
+
+
+/** -------------------------------------------------------
+ * 判断是否出现 " 操作成功 " 的 tip 层
+ * @var $session['isShowTip'] boolean  为 true 则出现 tip 层，false反之
+ */
+if ( $session['isShowTip'] ){
+
+	echo " <script>function tip(){ layer.msg('{$session['tipContent']}', { icon: 1, offset:'100px'}) }  </script>";
+	$session['isShowTip'] = false;
+}
+
+?>
+
+
+<div class="all">
+	<div class="bread-nav">
+		<span>图书搜索</span>  >
+		<span>图书编辑</span>
+	</div>
+
+
+<?php
+/**----------------------------------------------------
+ * 图书档案编辑 
+ */
+?>
+	<div class="input-box">
+		<?php $form = ActiveForm::begin();  ?> 
+			<?= Html::hiddenInput('bookInfoID', $bookInfoData -> PK_bookInfoID )  ?>
+			<?= $form->field( $model, 'bookInfoBookISBN') ->textinput(['placeholder' => 'ISBN', 'value' => $bookInfoData -> bookInfoBookISBN]) -> label( false ) ?>
+			<?= $form->field( $model, 'bookInfoBookName') ->textinput(['placeholder' => '图书名称', 'value' => $bookInfoData -> bookInfoBookName ]) -> label( false ) ?>
+
+			
+			<?= $form->field( $model, 'bookInfoBookAuthor') ->textinput(['placeholder' => '作者', 'value' => $bookInfoData -> bookInfoBookAuthor]) -> label( false ) ?>
+			<?= $form->field( $model, 'bookInfoBookTranslator') ->textinput(['placeholder' => '译者 (选填)', 'value' => $bookInfoData -> bookInfoBookTranslator]) -> label( false ) ?>
+
+
+
+			<?= $form->field( $model, 'bookInfoBookPrice') ->textinput(['placeholder' => '图书单价', 'value' => $bookInfoData -> bookInfoBookPrice]) -> label( false ) ?>
+			<?= $form->field( $model, 'bookInfoBookPage') ->textinput(['placeholder' => '图书页码', 'value' => $bookInfoData -> bookInfoBookPage]) -> label( false ) ?>
+
+			<div id="main" class="bookshelf">
+				<?= Html::dropDownList('bookshelf', null, $bookshelfData, ['class' => 'basic-usage-demo'  , 'value' => $bookRelsData->FK_bookshelfID]); ?>
+			</div>
+			<div class="help-block"></div> 
+
+			<div id="main" class="bookType">
+				<?= Html::dropDownList('bookType', null, $bookTypeData, ['class' => 'basic-usage-demo' , 'value' => $bookRelsData->FK_bookTypeID]); ?>
+			</div>
+			<div class="help-block"></div>
+
+			<div id="main" class="publisher">
+				<?= Html::dropDownList('publisher', null, $publisherData, ['class' => 'basic-usage-demo', 'value' => $bookRelsData->FK_publisherID ]); ?>
+			</div>
+			<div class="help-block"></div>
+
+			<?= Html::submitButton('更新', ['class' => 'btn btn-primary book-edit-btn'])	?>		
+		<?php ActiveForm::end()  ?>
+	</div>
+
+
+	<script>
+		window.onload = function()
+		{
+			dropDown();
+
+			selectOption('bookType');
+			selectOption('bookshelf');
+			selectOption('publisher');
+
+
+			tip();
+		}
+
+		function dropDown()
+		{
+			$(document).ready(function(){
+				$('.basic-usage-demo').fancySelect();	
+			});
+		}
+
+		
+	</script>
+
+
+
+
+</div>
