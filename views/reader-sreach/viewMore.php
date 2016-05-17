1ee6c2150b9a775e96c066af35502987d5913bc1
smartMao <664643875@qq.com>
2016-05-17 16:36:48 +0800

    增加 读者搜索中的 查看更多

---
+++ views/reader-sreach/viewMore.php
@@ -0,0 +1,63 @@
+<?php
+
+use app\assets\BookSreachAsset;
+
+BookSreachAsset::register( $this );
+
+?>
+
+
+<div class="all">
+	<div class="bread-nav">
+		<span>读者管理</span>  >
+		<span>读者搜索</span>  >
+		<span>查看更多</span>
+	</div>
+	
+	<button type="button" class="btn btn-primary view-more-goback-btn" onclick='history.go(-1);'> 返回 </button>
+
+
+	<table class='table table-bordered view-more-table text-center'>
+		<tbody>
+			<tr>
+				<td> 姓名 </td>
+				<td> <?php echo $data['readerName'];  ?> </td>
+			</tr>
+			<tr>
+				<td> 编号 </td>
+				<td> <?php echo $data['readerNumber'];  ?> </td>
+			</tr>
+			<tr>
+				<td> 出生年月 </td>
+				<td> <?php echo $data['readerBirthday'];  ?> </td>
+			</tr>
+			<tr>
+				<td> 证件类型 </td>
+				<td> <?php echo $data['readerCertificate'];  ?> </td>
+			</tr>
+			<tr>
+				<td> 证件号码 </td>
+				<td> <?php echo $data['readerCertificateNumber'];  ?> </td>
+			</tr>
+			<tr>
+				<td> 手机 </td>
+				<td> <?php echo $data['readerPhone'];  ?> </td>
+			</tr>
+			<tr>
+				<td> Email </td>
+				<td> <?php echo $data['readerEmail'];  ?> </td>
+			</tr>
+			<tr>
+				<td> 注册时间 </td>
+				<td> <?php echo $data['readerCreateDate'];  ?> </td>
+			</tr>
+		</tbody>	
+
+	</table>
+
+
+
+
+
+</div>
+
