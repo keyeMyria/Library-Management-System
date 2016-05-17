91353dbb29536d305e4b9fad3f29bcf078881796
smartMao <664643875@qq.com>
2016-05-17 22:39:58 +0800

    增加样式

--- views/book-info-stat/index.php
+++ views/book-info-stat/index.php
@@ -10,13 +10,18 @@ BookInfoStatAsset::register( $this );
 ?>
 <div class="all">
 	<div class="bread-nav">
-		<span>图书档案</span>
+		<span>图书档案</span> >
 		<span>图书信息统计</span>
 	</div>
 	<?= Html::hiddenInput('jsonData', $data , [ 'class' => 'jsonData' ] ) ?>	
 
 
-	<div id="container" style="min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto;"></div>	
+	<button type='button' class='btn btn-primary' > 书架统计 </button>
+	<button type='button' class='btn btn-primary' > 书架统计 </button>
+
+
+	<div class='help-block'></div>
+	<div id="container" class='chart' style="min-width: 310px; height: 520px; max-width: 700px; margin: 0 0 0 300px; "></div>	
 
 
 </div>
@@ -26,6 +31,7 @@ window.onload = function(){
 	str  = $('.jsonData').val();
 	json = eval("("+ str +")");
 	charts( json );
+
 }
 
 function charts( data ){
@@ -34,10 +40,12 @@ function charts( data ){
         chart: {
             plotBackgroundColor: null,
             plotBorderWidth: null,
-            plotShadow: false
+			plotShadow: false,
+			backgroundColor: '#FFFFF0',
+			padding: '0px',
         },
         title: {
-            text: 'Browser market shares at a specific website, 2014'
+            text: '图书馆内各书架内的书籍数量在图书馆内的占比'
         },
         tooltip: {
             pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
@@ -77,6 +85,9 @@ function charts( data ){
 
 
 }
+
+
+
 
 
 </script>
