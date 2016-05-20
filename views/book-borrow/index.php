04a97916944957ede8d9b410bc40a3453397c74e
smartMao <664643875@qq.com>
2016-05-20 11:27:16 +0800

    图书借阅 视图中的 显示 逻辑

--- views/book-borrow/index.php
+++ views/book-borrow/index.php
@@ -47,32 +47,66 @@ if ( $session['isShowTip'] ){
 	</div>
 	
 
+	<!--
+
+		1. 输入ISBN查询出具体的书
+		2. ISBN栏给上 class='isbn'
+		3. 用 js ， 当点击 “查询” 按钮后，去获取 class='isbn' 的 text,
+				如果有，就存放到 “确认借阅” 上方的隐藏域中，
+				如果没有，那就不用管。
+	 --> 
+
 	<table class='table table-hover table-book-info '>
 
 			<tbody>
+		<?php if( isset( $bookInfoData ) ){  ?>
+
 				<tr>
-				<td>ISBN:  &nbsp;<span> <?php echo $bookInfoData['bookInfoBookISBN'] ? $bookInfoData['bookInfoBookISBN'] : ' ';  ?>  </span></td>
-					<td>书名: <span>  《  <?php echo $bookInfoData['bookInfoBookName'] ? $bookInfoData['bookInfoBookName'] : ' ';  ?>  》  </span></td>
+				<td>ISBN:  &nbsp;<span class='isbn' book-info-id='<?php echo $bookInfoData['PK_bookInfoID']; ?>'> <?php echo $bookInfoData['bookInfoBookISBN'];  ?>  </span></td>
+					<td>书名: <span>  《  <?php echo $bookInfoData['bookInfoBookName'];  ?>  》  </span></td>
 				</tr>
 				<tr>
-				<td>作者: &nbsp;<span>    <?php echo $bookInfoData['bookInfoBookAuthor'] ? $bookInfoData['bookInfoBookAuthor'] : ' ';  ?></span></td>
-					<td>类型:  &nbsp;<span> <?php echo $bookInfoData['bookTypeName'] ? $bookInfoData['bookTypeName'] : ' ';  ?> </span></td>
+				<td>作者: &nbsp;<span>    <?php echo $bookInfoData['bookInfoBookAuthor'];  ?></span></td>
+					<td>类型:  &nbsp;<span> <?php echo $bookInfoData['bookTypeName'];  ?> </span></td>
 				</tr>
 				<tr>
-					<td>出版社:  <span>  <?php echo $bookInfoData['publisherName'] ? $bookInfoData['publisherName'] : ' ';  ?>  </span></td>
-					<td>书架:  &nbsp;<span> <?php echo $bookInfoData['bookshelfName'] ? $bookInfoData['bookshelfName'] : ' ';  ?> </span></td>
+					<td>出版社:  <span>  <?php echo $bookInfoData['publisherName'];  ?>  </span></td>
+					<td>书架:  &nbsp;<span> <?php echo $bookInfoData['bookshelfName'];  ?> </span></td>
 				</tr>
 				<tr>
-					<td>定价:  &nbsp;<span>  ￥<?php echo $bookInfoData['bookInfoBookPrice'] ? $bookInfoData['bookInfoBookPrice'] : ' ';  ?> </span></td>
-					<td>页码:  &nbsp;<span>  <?php echo $bookInfoData['bookInfoBookPage'] ? $bookInfoData['bookInfoBookPage'] : '未知 ';  ?></span></td>
+					<td>定价:  &nbsp;<span>  ￥<?php echo $bookInfoData['bookInfoBookPrice'];  ?> </span></td>
+					<td>页码:  &nbsp;<span>  <?php echo $bookInfoData['bookInfoBookPage'];  ?></span></td>
 				</tr> 
 
+
+		<?php   } else {  ?>
+			
+				<tr>
+				<td>ISBN:  &nbsp;<span>  </span></td>
+					<td>书名: <span>   </span></td>
+				</tr>
+				<tr>
+				<td>作者: &nbsp;<span> </span></td>
+					<td>类型:  &nbsp;<span> </span></td>
+				</tr>
+				<tr>
+					<td>出版社:  <span>    </span></td>
+					<td>书架:  &nbsp;<span>  </span></td>
+				</tr>
+				<tr>
+					<td>定价:  &nbsp;<span>   </span></td>
+					<td>页码:  &nbsp;<span>  </span></td>
+				</tr> 
+
+		<?php  }  ?>
+
 			</tbody>
+	</table>
 
-
-	<table>
-
-	<a href='#' class='btn btn-primary confirm-borrow-btn'> 确认借阅 </a>
+	<?= Html::beginForm( Url::to(['book-borrow/index']), 'post'  ) ?>	
+		<?= Html::hiddenInput( 'bookInfoID', null , ['id' => 'bookInfoID'] ); ?>
+		<?= Html::submitButton('确认借阅', ['class' => 'btn btn-primary confirm-borrow-btn', 'disabled' => 'true']) ?>
+	<?= Html::endForm() ?>
 	
 </div>
 
@@ -81,10 +115,21 @@ if ( $session['isShowTip'] ){
 
 window.onload = function()
 {
+	setBookInfoID();
 	tip();
 }
 
+// 当按照 ISBN 查询出图书信息，然后把 图书ID, 放入 “确认借阅” 的隐藏域中
+function setBookInfoID(){
 
+		bookInfoID = $('.isbn').attr('book-info-id');
+		$('#bookInfoID').val( bookInfoID );
+
+		if ( !isNaN( bookInfoID )  ){
+			// 不是　NaN, 说明有 ISBN ，把按钮 禁用的属性去掉
+			$('.confirm-borrow-btn').removeAttr('disabled');	
+		}
+}
 
 
 
