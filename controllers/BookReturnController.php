fcd22469c85a455048ddfb765bde9c9c96b7fbe5
smartMao <664643875@qq.com>
2016-05-21 14:20:40 +0800

    图书归还 模块 增加分页

--- controllers/BookReturnController.php
+++ controllers/BookReturnController.php
@@ -14,57 +14,90 @@ namespace app\controllers;
 
 use yii\web\Controller;
 use yii\web\Session;
+use yii\data\Pagination;
+use yii\helpers\Url;
 use Yii;
 
 use app\models\Reader;
 use app\models\BookReturn;
 
 
-
 class BookReturnController extends Controller
 {
 
+	# 借阅数据的分页条数 (每页)
+	public $defaultPageSize = 5;
+
 		
+	/*
+	 * 没有 Get 和 没有 Session 数据时，展示 “读者编号验证页面”
+	 * 有 Get 提交 或 Session 数据时，展示 “归还页面” （里面有 该读者信息，和他借阅了什么书）
+	 */
 	public function actionIndex()
 	{
 
 		$session = new Session;
 		$bookReturnModel = new BookReturn;
 
-		if ( isset( $session['readerNumber'] ) ){
-			// session 中的读者编号还在	
-			$readerData = $bookReturnModel -> getReaderInfo( $session['readerNumber'] );	
-			return $this -> render( 'index' , [
-				'session' => $session, 	
-				'readerData' => $readerData,
-			]);
-				
-		}
+		$connect = Yii::$app->db;
 
-		if ( $get = Yii::$app->request->get() ){
+		if ( $get = Yii::$app->request->get()  ){
+			
+			// 通过 get 进入到 图书归还页面
+			$readerNumber = $get['Reader']['readerNumber'];
 
-			$readerData = $bookReturnModel -> getReaderInfo( $get['Reader']['readerNumber'] );	
+			$readerData  = $bookReturnModel -> getReaderInfo( $connect, $readerNumber );	
 
 			if( $readerData ){
+
 				// 读者编号 验证成功
-				$session['readerNumber'] = $readerData -> readerNumber;								
+				$session['readerNumber'] = $readerData['readerNumber'];	
+				$session['readerID']     = $readerData['PK_readerID'];
 
 			} else {
 
 				// 读者编号 验证失败
-				$session['isShowTip'] = true;
+				$session['isShowTip']  = true;
 				$session['tipContent'] = '没有查询到此读者信息，请确认输入是否正确';
 				return $this->redirect('index'); // 等于跳回验证页面
 			}
 
+			// 根据此读者 ID, 查出借阅图书
+			$borrowQuery = $bookReturnModel -> getBorrowInfo( $connect, $readerData['PK_readerID'] );
+
+			$cloneQuery = clone $borrowQuery; $borrowedCount = $cloneQuery->count();
+
+			$pages = new Pagination(['totalCount' => $borrowedCount ]);
+			$pages -> defaultPageSize = $this -> defaultPageSize;
+
+			$models = $borrowQuery -> offset( $pages->offset ) 
+					               -> limit(  $pages->limit ) 
+					               -> all();
+
 			return $this -> render( 'index' , [
-				'session' => $session, 	
-				'readerData' => $readerData,
+
+				'pages'         => $pages,	
+				'models'        => $models,
+				'session'       => $session, 	
+				'readerData'    => $readerData,
+				'borrowedCount' => $borrowedCount,
 			]);
 
+
+		} elseif( isset( $session['readerNumber']) ){
+
+			// 通过 Session 进入 “归还页面”  ( 但始终都是跳转回 get 提交，因为要分页呀)
+		
+			$url = 'book-return/index?Reader[readerNumber]=' . $session['readerNumber'] . '&page=1' ;
+
+			# 用完 $session['readerNumber'] 后，注销掉，避免重复跳转
+			unset( $session['readerNumber'] );
+			return $this -> redirect([$url]);	
+
 		} else {
-
-
+			
+			// 没有 Get 数据 也没有 Session 数据, 显示 “读者验证页面”
+			
 			$readerModel = new Reader;
 			return $this -> render('verify', [
 				'readerModel' => $readerModel,		
