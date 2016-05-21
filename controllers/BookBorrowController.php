f8810bf824939a48a9d22c1b39e91e592986bef0
smartMao <664643875@qq.com>
2016-05-21 20:02:15 +0800

    完善借阅模块的边界限制，

--- controllers/BookBorrowController.php
+++ controllers/BookBorrowController.php
@@ -37,21 +37,22 @@ class BookBorrowController extends Controller
 
 			// 在 图书借阅 页面 点击 “ 确认借阅 ” 按钮	
 
-			$timeData = $bookBorrowModel -> borrowTimestamp( $this->borrowCycle  );
-			
-			$bookBorrowModel -> FK_bookInfoID         = $post['bookInfoID'];
-			$bookBorrowModel -> FK_readerID           = $session['readerID'];
-			$bookBorrowModel -> borrowBeginTimestamp  = $timeData['beginTime'];
-			$bookBorrowModel -> borrowReturnTimestamp = $timeData['endTime'];
-			$bookBorrowModel -> borrowIsReturn        = 0;
+			$result = $this -> confirmBorrow( $post );
 
-			if( $bookBorrowModel -> save() ){
-				$session['isShowTip'] = true;	
+			$session['isShowTip'] = true;	
+			if( $result == 1 ){
+
 				$session['tipContent'] = '借阅成功';
 				$session['tipLevel']   = 1;
-				return $this -> redirect('index');	
-			}
+
+			} elseif( $result == 2 ){
+
+				$session['tipContent'] = '超过借阅数量限制,请归还部分书籍后再尝试借阅';
+				$session['tipLevel']   = 2;
 			
+			}
+
+			return $this -> redirect('index');	
 		}
 
 		if( $get = $request->get() ){
@@ -96,21 +97,52 @@ class BookBorrowController extends Controller
 	}	
 
 
+
+
+
+
+	/**
+	 * 从 actionIndex() 分出来的功能, 归属与 actionIndex() 的 post 提交处理部分
+	 * 当 在图书借阅页面 查询出了 图书信息之后  点击了 “确认借阅 ” 按钮
+	 */
+	public function confirmBorrow( $post )
+	{
+		$bookBorrowModel = new BookBorrow;
+		$session         = new Session;
+		$connect = Yii::$app->db;
+
+		$readerID = $session['readerID'];
+		$allowBorrowNumber = $bookBorrowModel -> getAllowBorrowNumber( $connect , $readerID );
+
+		$borrowedCount     = BookBorrow::find() -> where(['borrowIsReturn' => '0']) -> count();
+
+		$isAllowBorrow = $allowBorrowNumber - $borrowedCount;
+
+		if( $isAllowBorrow ){
+
+			$timeData = $bookBorrowModel -> borrowTimestamp( $this->borrowCycle  );
+					
+			$bookBorrowModel -> FK_bookInfoID         = $post['bookInfoID'];
+			$bookBorrowModel -> FK_readerID           = $session['readerID'];
+			$bookBorrowModel -> borrowBeginTimestamp  = $timeData['beginTime'];
+			$bookBorrowModel -> borrowReturnTimestamp = $timeData['endTime'];
+			$bookBorrowModel -> borrowIsReturn        = 0;
+			
+			if( $bookBorrowModel -> save() ){
+				// 借阅成功
+				return 1;	
+			}
+		} else {
+			// 该用户借阅图书数量超过限制
+			return 2;	
+		}
+	
+	}
+
+
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 ?>
